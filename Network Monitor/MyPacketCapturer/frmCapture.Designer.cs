dd9a3f9819b142fee29e378a15f7d843888464d8
superspades <superspades@users.noreply.github.com>
2016-08-31 09:28:35 -0400

    Add files via upload

---
+++ Network Monitor/MyPacketCapturer/frmCapture.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace MyPacketCapturer
+{
+    partial class frmCapture
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            this.btnStartStop = new System.Windows.Forms.Button();
+            this.cmbDevices = new System.Windows.Forms.ComboBox();
+            this.txtCapturedData = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.screenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.txtNumPackets = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtGUID = new System.Windows.Forms.TextBox();
+            this.ipList = new System.Windows.Forms.TextBox();
+            this.dataTraffic = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.chartBtn = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataTraffic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStartStop
+            // 
+            this.btnStartStop.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnStartStop.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStartStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStartStop.Location = new System.Drawing.Point(16, 39);
+            this.btnStartStop.Margin = new System.Windows.Forms.Padding(4);
+            this.btnStartStop.Name = "btnStartStop";
+            this.btnStartStop.Size = new System.Drawing.Size(113, 43);
+            this.btnStartStop.TabIndex = 0;
+            this.btnStartStop.Text = "Start";
+            this.btnStartStop.UseVisualStyleBackColor = false;
+            this.btnStartStop.Click += new System.EventHandler(this.btnStartStop_Click);
+            // 
+            // cmbDevices
+            // 
+            this.cmbDevices.FormattingEnabled = true;
+            this.cmbDevices.Location = new System.Drawing.Point(16, 94);
+            this.cmbDevices.Margin = new System.Windows.Forms.Padding(4);
+            this.cmbDevices.Name = "cmbDevices";
+            this.cmbDevices.Size = new System.Drawing.Size(498, 24);
+            this.cmbDevices.TabIndex = 1;
+            this.cmbDevices.SelectedIndexChanged += new System.EventHandler(this.cmbDevices_SelectedIndexChanged);
+            // 
+            // txtCapturedData
+            // 
+            this.txtCapturedData.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCapturedData.Location = new System.Drawing.Point(16, 156);
+            this.txtCapturedData.Margin = new System.Windows.Forms.Padding(4);
+            this.txtCapturedData.Multiline = true;
+            this.txtCapturedData.Name = "txtCapturedData";
+            this.txtCapturedData.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtCapturedData.Size = new System.Drawing.Size(498, 514);
+            this.txtCapturedData.TabIndex = 2;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.screenToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(8, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(1509, 28);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToolStripMenuItem,
+            this.openToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(44, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(120, 26);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(120, 26);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // screenToolStripMenuItem
+            // 
+            this.screenToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.clearToolStripMenuItem});
+            this.screenToolStripMenuItem.Name = "screenToolStripMenuItem";
+            this.screenToolStripMenuItem.Size = new System.Drawing.Size(65, 24);
+            this.screenToolStripMenuItem.Text = "Screen";
+            // 
+            // clearToolStripMenuItem
+            // 
+            this.clearToolStripMenuItem.Name = "clearToolStripMenuItem";
+            this.clearToolStripMenuItem.Size = new System.Drawing.Size(118, 26);
+            this.clearToolStripMenuItem.Text = "Clear";
+            this.clearToolStripMenuItem.Click += new System.EventHandler(this.clearToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // txtNumPackets
+            // 
+            this.txtNumPackets.Location = new System.Drawing.Point(389, 65);
+            this.txtNumPackets.Margin = new System.Windows.Forms.Padding(4);
+            this.txtNumPackets.Name = "txtNumPackets";
+            this.txtNumPackets.Size = new System.Drawing.Size(125, 22);
+            this.txtNumPackets.TabIndex = 4;
+            this.txtNumPackets.Text = "0";
+            this.txtNumPackets.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(386, 44);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Number of Packets";
+            // 
+            // txtGUID
+            // 
+            this.txtGUID.Location = new System.Drawing.Point(16, 126);
+            this.txtGUID.Margin = new System.Windows.Forms.Padding(4);
+            this.txtGUID.Name = "txtGUID";
+            this.txtGUID.Size = new System.Drawing.Size(498, 22);
+            this.txtGUID.TabIndex = 6;
+            // 
+            // ipList
+            // 
+            this.ipList.Location = new System.Drawing.Point(535, 156);
+            this.ipList.Multiline = true;
+            this.ipList.Name = "ipList";
+            this.ipList.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.ipList.Size = new System.Drawing.Size(173, 514);
+            this.ipList.TabIndex = 7;
+            // 
+            // dataTraffic
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.dataTraffic.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.dataTraffic.Legends.Add(legend1);
+            this.dataTraffic.Location = new System.Drawing.Point(731, 53);
+            this.dataTraffic.Name = "dataTraffic";
+            this.dataTraffic.Size = new System.Drawing.Size(758, 617);
+            this.dataTraffic.TabIndex = 8;
+            this.dataTraffic.Text = "Data Trafic";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(532, 136);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 17);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "IP Address";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(614, 136);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 17);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Data Bytes";
+            // 
+            // chartBtn
+            // 
+            this.chartBtn.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.chartBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.chartBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chartBtn.Location = new System.Drawing.Point(557, 53);
+            this.chartBtn.Name = "chartBtn";
+            this.chartBtn.Size = new System.Drawing.Size(134, 43);
+            this.chartBtn.TabIndex = 11;
+            this.chartBtn.Text = "Display Chart";
+            this.chartBtn.UseVisualStyleBackColor = false;
+            this.chartBtn.Click += new System.EventHandler(this.chartBtn_Click);
+            // 
+            // frmCapture
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.ClientSize = new System.Drawing.Size(1509, 682);
+            this.Controls.Add(this.chartBtn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataTraffic);
+            this.Controls.Add(this.ipList);
+            this.Controls.Add(this.txtGUID);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNumPackets);
+            this.Controls.Add(this.txtCapturedData);
+            this.Controls.Add(this.cmbDevices);
+            this.Controls.Add(this.btnStartStop);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "frmCapture";
+            this.Text = "Packet Capture";
+            this.Load += new System.EventHandler(this.frmCapture_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataTraffic)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStartStop;
+        private System.Windows.Forms.ComboBox cmbDevices;
+        private System.Windows.Forms.TextBox txtCapturedData;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem screenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clearToolStripMenuItem;
+        private System.Windows.Forms.TextBox txtNumPackets;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtGUID;
+        private System.Windows.Forms.TextBox ipList;
+        private System.Windows.Forms.DataVisualization.Charting.Chart dataTraffic;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button chartBtn;
+    }
+}
+
---
+++ Network Monitor/MyPacketCapturer/frmCapture.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PacketDotNet;
+using SharpPcap;
+using System.Collections;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace MyPacketCapturer
+{
+    public partial class frmCapture : Form
+    {
+
+        CaptureDeviceList devices;   //List of devices for this computer
+        public static ICaptureDevice device; //The device we will be using
+        public static string stringPackets = ""; //Data that is captured
+        static int numPackets = 0;
+        string filter = "ip and tcp"; //filter string
+        static int length = 0; //size of packet
+        static int count = 0; //tracking size of dictionary
+        public static string sIP = ""; //source IP address of packet
+        public static string dIP = ""; //destination IP aaddress of packet
+        //create a dictionary to store IP addresses and sizes of packets
+        public static Dictionary<string, int> IPvalues = new Dictionary<string, int>();
+
+
+//**********Default constructor
+public frmCapture()
+        {
+            InitializeComponent();
+            
+            //Get the list of devices
+            devices = CaptureDeviceList.Instance;
+
+            //Make sure that there is at least one device
+            if (devices.Count < 1)
+            {
+                MessageBox.Show("No Capture Devices Found!!!");
+                Application.Exit();
+            }
+
+            //Add the devices to the combo box
+            foreach (ICaptureDevice dev in devices)
+            {
+                cmbDevices.Items.Add(dev.Description);
+            }
+
+            //Get the second device and display in combo box
+            device = devices[0];
+            cmbDevices.Text = device.Description;
+
+            //Register our handler function to the 'packet arrival' event
+            device.OnPacketArrival += new SharpPcap.PacketArrivalEventHandler(device_OnPacketArrival);
+
+            //Open the device for capturing
+            int readTimeoutMilliseconds = 1000;
+            device.Open(DeviceMode.Promiscuous, readTimeoutMilliseconds);
+            device.Filter = filter;
+        } //End frmCapture
+
+
+        //********** Event handler when a packet arrives
+        private static void device_OnPacketArrival(object sender, CaptureEventArgs packet)
+        {
+            //Increment the number of packets captured
+            numPackets++;
+
+            //Put the packet number in the capture window
+            stringPackets += "Packet Number: " + Convert.ToString(numPackets);
+            stringPackets += Environment.NewLine;
+
+            //Array to store our data
+            byte[] data = packet.Packet.Data;
+
+            //Keep track of the number of bytes displayed per line
+            int byteCounter = 0;
+
+
+            stringPackets += "Destination MAC Address: ";
+            //Parsing the packets
+            foreach (byte b in data)
+            {
+                //Add the byte to our string (in hexidecimal)
+                if(byteCounter<=13||byteCounter>=34&&byteCounter<=37) stringPackets += b.ToString("X2") + " ";
+                byteCounter++;
+                length++; //increment lenght for each processed by byte
+                switch (byteCounter)
+                {
+                    case 6: stringPackets += Environment.NewLine;
+                        stringPackets += "Source MAC Address: ";
+                        break;
+                    case 12: stringPackets += Environment.NewLine;
+                        stringPackets += "EtherType: ";
+                        break;
+                    case 14: if (data[12] == 8)
+                        {
+                            if (data[13] == 0) stringPackets += "(IP)";
+                            if (data[13] == 6) stringPackets += "(ARP)";
+                        }
+                        break;
+                    case 26: stringPackets += Environment.NewLine; //parsing source IP
+                        stringPackets += "Source IP: ";
+                        stringPackets += data[26] + "." + data[27] + "." + data[28] + "." + data[29];
+                        sIP = data[26] + "." + data[27] + "." + data[28] + "." + data[29];
+                        break;
+                    case 30: stringPackets += Environment.NewLine; //parsing destination IP
+                        stringPackets += "Destination IP: ";
+                        stringPackets += data[30] + "." + data[31] + "." + data[32] + "." + data[33];
+                        dIP = data[30] + "." + data[31] + "." + data[32] + "." + data[33];
+                        break;
+                    case 34: stringPackets += Environment.NewLine; //source port in hex
+                        stringPackets += "Source Port: ";
+                        break;
+                    case 36: stringPackets += Environment.NewLine; //destination port in hex
+                        stringPackets += "Destination Port: ";
+                        break;
+                }
+            }
+
+            stringPackets += Environment.NewLine;
+            stringPackets += "Packet Length: " + length + " Bytes";
+            stringPackets += Environment.NewLine + Environment.NewLine;
+
+            //populate the dictionary with IP addresses and coresponding packet size
+            //restrain recorded IPs to class B @ C networks to reduce graph size
+            if (data[26] > 100)
+            {
+                if (!IPvalues.ContainsKey(sIP))
+                {
+                    IPvalues.Add(sIP, length);
+                    count++;
+
+                }
+                else IPvalues[sIP] += length;
+            }
+            if(data[30] > 100)
+            {
+                if (!IPvalues.ContainsKey(dIP))
+                {
+                    IPvalues.Add(dIP, length);
+                    count++;
+                }
+                else IPvalues[dIP] += length;
+            }
+            
+
+            byteCounter = 0;
+            length = 0;
+            sIP = "";
+            dIP = "";
+        } //End device_OnPacketArrival
+
+
+        //**********Starting and stopping the packet capturing
+        private void btnStartStop_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (btnStartStop.Text == "Start")
+                {
+                    btnStartStop.Text = "Stop";
+                    device.StartCapture();
+                    timer1.Enabled = true;
+                  }
+                else
+                {
+                    btnStartStop.Text = "Start";
+                    device.StopCapture();
+                    timer1.Enabled = false;
+                }
+
+            }
+            catch (Exception exp) { MessageBox.Show("Error starting and stopping capture"); }
+        } //End btnStartStop
+
+
+        //**********Dumping the packet data from stringPackets to the text box
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            txtCapturedData.AppendText(stringPackets);
+            stringPackets = "";
+            txtNumPackets.Text = Convert.ToString(numPackets);
+            
+            //write values of IP dictionary to ipList textbox
+            var lines = IPvalues.Select(t => t.Key +"\t"+ t.Value);
+            ipList.Text = string.Join(Environment.NewLine, lines);
+
+        } //End timer1
+
+
+        //**********Changing devices
+        private void cmbDevices_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (device != null) device.Close();
+            device = devices[cmbDevices.SelectedIndex];
+            cmbDevices.Text = device.Description;
+            txtGUID.Text = device.Name;
+
+            //Register our handler function to the 'packet arrival' event
+            device.OnPacketArrival += new SharpPcap.PacketArrivalEventHandler(device_OnPacketArrival);
+
+            //Open the device for capturing
+            int readTimeoutMilliseconds = 1000;
+            device.Open(DeviceMode.Promiscuous, readTimeoutMilliseconds);
+        } //End cmbDevices_SelectedIndexChanged
+
+ 
+        //**********Saving the file
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter = "Text Files|*.txt|All Files|*.*";
+            saveFileDialog1.Title = "Save the Captured Packets";
+            saveFileDialog1.ShowDialog();
+
+            //Check to see if a filename was given
+            if (saveFileDialog1.FileName != "")
+            {
+                System.IO.File.WriteAllText(saveFileDialog1.FileName, txtCapturedData.Text);
+            }
+        } //End saveToolStripMenuItem_Click
+
+
+        //**********Openning the file
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Text Files|*.txt|All Files|*.*";
+            openFileDialog1.Title = "Open Captured Packets";
+            openFileDialog1.ShowDialog();
+
+            //Check to see if a filename was given
+            if (openFileDialog1.FileName != "")
+            {
+                txtCapturedData.Text=System.IO.File.ReadAllText(openFileDialog1.FileName);
+            }
+        } //End openToolStripMenuItem_Click
+
+        //**********Clear the main textbox
+        private void clearToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            txtCapturedData.Clear();
+            ipList.Clear();
+            numPackets = 0;
+            txtNumPackets.Text = "0";
+        }  //End learToolStripMenuItem_Click
+
+        private void frmCapture_Load(object sender, EventArgs e)
+        {
+            dataTraffic.ChartAreas["ChartArea1"].AxisX.Interval = 1;
+        }
+
+        private void chartBtn_Click(object sender, EventArgs e)
+        {
+            //assign the IP addresses and packet sizes from the dictionary to the chart
+
+            try {
+                if (chartBtn.Text == "Display Chart")
+                {
+                    chartBtn.Text = "Clear Chart";
+                    Series ser1 = new Series(" Network\n Traffic\n Per IP \n In Bytes", 10);
+                    dataTraffic.Series.Add(ser1);
+                    dataTraffic.Series[" Network\n Traffic\n Per IP \n In Bytes"].Points.DataBindXY(IPvalues.Keys, IPvalues.Values);
+                }
+                else {
+                    chartBtn.Text = "Display Chart";
+                    dataTraffic.Series.Clear();
+                    IPvalues.Clear();
+                }
+            }catch(Exception exp) { MessageBox.Show("Problem With Chart Display"); }
+        }
+    }
+}
